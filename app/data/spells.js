9c1df1b142831ff214054429b838e5f6c851033a
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2019-02-11 13:50:25 -0700

    Cloned HotRestaurant as template for SpellFinder

---
+++ app/data/spells.js
@@ -0,0 +1,53 @@
+let spells = [
+    {
+        name: "Patronus Charm (Expecto Patronum)",
+        photo: "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/19/17/enhanced/webdr02/original-grid-image-23059-1447970713-6.jpg?downsize=700:*&output-format=auto&output-quality=auto",
+        scores: [
+            "5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+        ]
+    },
+    {
+        name: "Summoning Charm (Accio)",
+        photo: "http://images6.fanpop.com/image/photos/39800000/Hermione-Holding-Wand-hermione-granger-39882509-1024-768.jpg",
+        scores: [
+            "3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+        ]
+    },
+    {
+        name: "Levitation Charm (Wingardium Leviosa)",
+        photo: "http://images5.fanpop.com/image/photos/27800000/Ronald-Weasley-ronald-weasley-27814621-1280-960.jpg",
+        scores: [
+            "3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"
+        ]
+    }
+    
+];
+
+module.exports = spells;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,57 @@
+// The apiRoutes.js file includes two basic routes for app.get function and app.post function which used for displaying a JASON data and incoming survey results of all possible spells:
+// The app.post(in the apiRoutes.js) used to handle the compatibility logic //
+
+let spells = require("../data/spells.js");
+
+// Routing the apiRoutes with the app.get and app.post functions //
+module.exports = function (app) {
+    // The app.get request handles when user 'visits' a page //
+    app.get("/api/spells", function (req, res) {
+        res.json(spells);
+    });
+    // The app.post request handles when a user submits a form and thus submits data to the server //
+    app.post("/api/spells", function (req, res) {
+        // Loop through all of the possible options //
+        let bestMatch = {
+            name: "",
+            photo: "",
+            spellDifference: 1000
+        };
+
+        // To take the result of the user's survey POST and parse it //
+        let userData = req.body;
+        let userScores = userData.scores;
+        // To take the results of the user's name and photo, other than the survey questions, to post and parse it //
+        let userName = userData.name;
+        let userPhoto = userData.photo;
+
+        // The variable used to calculate the difference b/n the user's socres and the scores of each user //
+        let totalDifference = 0;
+
+        // Loop through the spells data array of objects to get each spells scores //
+        for (var i = 0; i < spells.length - 1; i++) {
+            console.log(spells[i].name);
+            totalDifference = 0;
+
+            // Loop through that spells score and the users score and calculate the absolute difference between the two and push that to the total difference variable set above //
+            for (var j = 0; j < 10; j++) {
+                // We calculate the difference between the scores and sum them into the totalDifference //
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(spellss[i].scores[j]));
+                // If the sum of differences is less then the differences of the current "best match" //
+                if (totalDifference <= bestMatch.spellDifference) {
+
+                    // Reset the bestMatch to be the new spell //
+                    bestMatch.name = spells[i].name;
+                    bestMatch.photo = spells[i].photo;
+                    bestMatch.spellDifference = totalDifference;
+                }
+            }
+        }
+
+        // The push method is used to save user's data to the database //
+        spells.push(userData);
+
+        // The res.json method will return a JSON data with the user's match which was looped through spells data array //
+        res.json(bestMatch);
+    });
+};
