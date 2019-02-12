ad2f2be46c987889213999813958df0c0670c2b8
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2019-02-11 22:00:19 -0700

    added  spells

--- app/data/spells.js
+++ app/data/spells.js
@@ -48,10 +48,83 @@ let spells = [
         ]
     },
     {
-        name: "",
-        photo: "https://i.imgur.com/XM48KAL.jpg";
+        name: "Disarming Charm (Expelliarmus)",
+        photo: "https://i.imgur.com/XM48KAL.jpg",
         scores: [
-            
+            "3",
+            "4",
+            "3",
+            "3",
+            "2",
+            "2",
+            "2",
+            "3",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Killing Curse (Avada Kedavra)",
+        photo: "https://i.stack.imgur.com/KdvZk.jpg",
+        scores: [
+            "2",
+            "5",
+            "2",
+            "3",
+            "3",
+            "4",
+            "3",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Curse (Sectumsempra)",
+        photo: "https://boygeniusreport.files.wordpress.com/2016/01/alan-rickman-snape.jpg?quality=98&strip=all&w=782",
+        scores: [
+            "5",
+            "4",
+            "4",
+            "2",
+            "4",
+            "3",
+            "5",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: "Memory Charm (Obliviate)",
+        photo: "https://i4.lisimg.com/11863064/411full.jpg",
+        scores: [
+            "2",
+            "3",
+            "4",
+            "4",
+            "2",
+            "3",
+            "3",
+            "3",
+            "2",
+            "2"
+        ]
+    },
+    {
+        name: "Charm (Riddikulus)",
+        photo: "https://images.pottermore.com/bxd3o8b291gf/2EykcsjRssY8aiKCgGgO2U/67c28c246b5c7f146f755d8bd225d0e4/DracoMalfoy_WB_F8_DracoMalfoyPromoShot_Promo_100615_Port.jpg?w=1200",
+        scores: [
+            "4",
+            "1",
+            "2",
+            "3",
+            "3",
+            "4",
+            "4",
+            "3",
+            "2",
+            "2"
         ]
     }
     
